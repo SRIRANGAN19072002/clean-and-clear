70f809523a1d930874c89e53ba4eb8f9039d110f
unknown <info.srirangan@gmail.com>
2024-02-16 04:46:58 -0800

    This is version 1

---
+++ app.js
@@ -0,0 +1,7 @@
+function change(){
+    let changed=document.getElementById('img')
+    changed.src="backpacker-standing-sunrise-viewpoint-ja-bo-village-mae-hong-son-province-thailand.jpg"
+ 
+
+
+}
